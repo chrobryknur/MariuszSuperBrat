ae0ea44244f38b32c44e9313d2c26b7254c6f5ed
Marcin Dąbrowski <chrobryknur@gmail.com>
2020-06-19 00:36:55 +0200

    Zmiana bloku ifów na .collides

--- Classes/Menu.py
+++ Classes/Menu.py
@@ -5,6 +5,7 @@ class Menu:
         self.surface = pygame.display.set_mode(wsize)
         pygame.display.set_caption('Mariusz Super Brat')
         self.initiated = False
+        self.handle=True
 
     def text_sufrace(self,text, font,color):
         white = (255, 255, 255)
@@ -23,39 +24,36 @@ class Menu:
         self.title =       [pygame.Rect(100, 100, 600, 100),(0,0,0), self.text_sufrace("Mariusz Super Brat", self.titleText, (0,96,128))]
         self.game_button = [pygame.Rect(300, 300, 200, 50), (0,0,0), self.text_sufrace("Start game", self.buttonText, (0,128,0))]
         self.quit_button = [pygame.Rect(300, 400, 200, 50), (0,0,0), self.text_sufrace("Quit game", self.buttonText, (0,128,0))]
+        self.clock = pygame.time.Clock()
+
 
     def draw(self,screen):
         if not self.initiated:
             self.init()
             self.initiated = True
 
-        clock = pygame.time.Clock()
-
-        handle=True
-        while handle:
+        while self.handle:
             self.surface.blit(self.background,(0,0))
             for event in pygame.event.get():
-                mouse_pos = pygame.mouse.get_pos()
                 if event.type == pygame.locals.QUIT:
-                    handle=False
-                    return 0
+                    self.handle=False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if 300 <= mouse_pos[0] <= 500:
-                        if 300 <= mouse_pos[1] <= 350:
+                    if event.button == 1:
+                        if self.game_button[0].collidepoint(event.pos):
                             self.next_s = 1
-                            handle = False
-                        if 400 <= mouse_pos[1] <= 450:
+                            self.handle = False
+                        if self.quit_button[0].collidepoint(event.pos):
                             self.next_s = -1
-                            handle = False
-                if 300 <= mouse_pos[0] <= 500:
-                    if 300 <= mouse_pos[1] <= 350:
-                        self.game_button[1] = (255, 0, 0)
-                    else:
-                        self.game_button[1] = (0, 0, 0)
-                    if 400 <= mouse_pos[1] <= 450:
-                        self.quit_button[1] = (255, 0, 0)
-                    else:
-                        self.quit_button[1] = (0, 0, 0)
+                            self.handle = False
+                if self.game_button[0].collidepoint(pygame.mouse.get_pos()):
+                    self.game_button[1] = (255, 0, 0)
+                else:
+                    self.game_button[1] = (0, 0, 0)
+                if self.quit_button[0].collidepoint(pygame.mouse.get_pos()):
+                    self.quit_button[1] = (255, 0, 0)
+                else:
+                    self.quit_button[1] = (0, 0, 0)
+
             self.game_button[2] = self.text_sufrace("Start game", self.buttonText, self.game_button[1])
             self.quit_button[2] = self.text_sufrace("Quit game", self.buttonText, self.quit_button[1])
             pygame.draw.rect(self.surface, self.game_button[1], self.game_button[0], 5)
@@ -66,5 +64,5 @@ class Menu:
             screen.blit(self.surface,(0,0))
 
             pygame.display.update()
-            clock.tick(15)
+            self.clock.tick(15)
 
